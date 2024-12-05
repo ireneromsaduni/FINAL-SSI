3c16936c59e047ed7ed120ac29e7c05832544a40
Irene Romero Sadaba <romero.146734@e.unavarra.es>
2024-12-05 16:20:43 +0100

    Update script.sh

--- ARCHIVOS/script.sh
+++ ARCHIVOS/script.sh
@@ -7,6 +7,13 @@ systemctl start apache2
 # Crear usuario www-data si no existe
 id -u www-data &>/dev/null || useradd -m www-data
 
+# Asegurarse de que el directorio home de www-data exista
+if [ ! -d "/home/www-data" ]; then
+    mkdir /home/www-data
+    chown www-data:www-data /home/www-data
+    chmod 700 /home/www-data
+fi
+
 # Generar y escribir la flag para www-data
 FLAG=$(echo "ssi{$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 20)}")
 echo "$FLAG" > /home/www-data/user.txt
